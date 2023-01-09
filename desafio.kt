1e9281f3ad00369232a286cfca93266094487c30
Juan Pablo <70600479+juanpb1@users.noreply.github.com>
2023-01-08 21:24:18 -0300

    Ajustes na Class Enum e Usuario e Adicionado a função de printar o usuário

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,73 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+interface PrintNivelUsario{
+    fun printNivel() : String 
+}
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel : PrintNivelUsario { 
+    BASICO{
+        override fun printNivel() = "Básico"
+    }
+    , INTERMEDIARIO{
+         override fun printNivel() = "Intermediário"
+    }, AVANCADO{
+         override fun printNivel() = "Avançado"
+    } 
+}
 
-class Usuario
+data class Usuario(val nome: String, val id: Int){
+    constructor() : this("", 0)
+}
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60){
+    constructor() : this("", 0)
+}
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        
+        inscritos.add(usuario)
+      
+    }
+}
+
+fun printUsuarios(formacao: Formacao){
+    print("= ALUNO(S): ")
+    for(inscrito in formacao.inscritos){
+        print("${inscrito.nome} | ")
+    }
+    println("")
+    println("= FORMACAO: ${formacao.nome}")
+    println("= NÍVEL: ${Nivel.BASICO.printNivel()}")
+    println("= CONTEÚDOS: ")
+    for(conteudo in formacao.conteudos){
+        println("Nome: ${conteudo.nome} | Duração(h): ${conteudo.duracao}")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
-}
+    
+    val aluno0 = Usuario("Pedro", 1)
+    val aluno1 = Usuario("Marcos", 2)
+    
+    val conteudoEducacional0 = ConteudoEducacional("Introdução Prática à Linguagem de Programação Kotlin", 30)
+    val conteudoEducacional1 = ConteudoEducacional("Estruturas de Controle de Fluxo e Coleções em Kotlin", 60)
+    val conteudos = mutableListOf<ConteudoEducacional>()
+    conteudos.add(conteudoEducacional0)
+    conteudos.add(conteudoEducacional1)
+    
+    val nomeFormacao = "Kotlin Experiencen"
+    val formacao0 = Formacao(nomeFormacao, conteudos)
+    
+    formacao0.matricular(aluno0)
+    formacao0.matricular(aluno1)
+    
+    printUsuarios(formacao0)
+    println()
+        
+  }
+    
+    
+    
+    
